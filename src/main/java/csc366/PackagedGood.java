e12bdd3a881a9c8e6379c2cca41166e1dbaef65b
Nathan Holland <nwhollan@calpoly.edu>
2022-03-03 09:20:06 -0800

    Created tables, all relationships not done yet though

---
+++ src/main/java/csc366/PackagedGood.java
@@ -0,0 +1,55 @@
+package csc366;
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.*;
+
+
+@Entity  
+@Table(
+    name = "PackagedGood",
+    uniqueConstraints = @UniqueConstraint(columnNames={"PackagedGoodsId"})
+)
+
+public class PackagedGood {
+    @Column(name="PackagedGoodId")
+    private int packagedGoodId;
+
+    @Column(name="Name")
+    private String name;
+
+    public PackagedGood(int packagedGoodId, String name) {
+        this.packagedGoodId = packagedGoodId;
+        this.name = name;
+    }
+
+    public int getPackagedGoodId() {
+        return packagedGoodId;
+    }
+    public void setPackagedGoodId(int packagedGoodId) {
+        this.packagedGoodId = packagedGoodId;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PackagedGood pg = (PackagedGood) o;
+        return packagedGoodId == pg.packagedGoodId
+            && name == pg.name;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(packagedGoodId, name);
+    }
+}
